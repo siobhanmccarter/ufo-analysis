e0324ad53d0b1ff44c7979d751ef8eb03cf6f215
Siobhan McCarter <siobhan.mccarter@gmail.com>
2017-12-05 11:58:30 -0800

    Adding the general structure for my v2.0 scripts

--- src/analysis.R
+++ src/analysis.R
@@ -1,18 +1,10 @@
 # Data analysis script
 
-## Loading dependencies
-
-library(tidyverse)
-library(lubridate)
-library(ggmap)
-
-
-scrubbed <- read_csv("data/scrubbed.csv")
+# filter to just the US
+us <- scrubbed %>% 
+  filter(country == "us")
 
 # checking the count of sightings per state
-usstate <- scrubbed %>%
-  filter(country == "us") %>% 
+usstate <- us %>%  
   group_by(state) %>% 
   summarise(count = n())
-
-map_data("state")
---
+++ ufo-analysis/.Rproj.user/4FE0A75A/sources/s-8CF96D93/8D1B50E5-contents
@@ -0,0 +1,9 @@
+## Loading dependencies
+
+library(tidyverse)
+library(ggmap)
+
+# Importing the data
+scrubbed <- read_csv("https://raw.githubusercontent.com/siobhanmccarter/ufo-analysis/master/ufo-analysis/data/scrubbed.csv")
+
+states <- read_csv()#PUTLINKHERE)
